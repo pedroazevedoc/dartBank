40d123c996fbd487b816b893506bdd5a917ce03a
Pedro Azevedo <pedroazvdo.8@gmail.com>
2026-02-03 22:11:56 -0300

    feat: finalizing addition the transaction methods in menu

--- bin/bank.dart
+++ bin/bank.dart
@@ -42,7 +42,8 @@ Map<String, Function> menuOptions = {
     print("Welcome to Dart Bank! What do you need?"),
     print("[1] Open an account"),
     print("[2] Make a transaction"),
-    print("[3] View accounts"),
+    print("[3] Calculate yield of savings account"),
+    print("[4] View accounts"),
     print("[0] Leave the bank"),
   },
   'account-open': () => {
@@ -58,12 +59,14 @@ Map<String, Function> menuOptions = {
     print("Which transaction you'll make?"),
     print("[1] Make a withdrawal"),
     print("[2] Deposit"),
-    print("[3] Calculate yield"),
+    print("[3] Deposit in salary account"),
     print("[0] Back to menu"),
   },
 };
 
-// Menu actions
+/*
+ * Menu actions
+ */
 void openActions() {
   int command = 9;
   while (command != 0) {
@@ -100,20 +103,20 @@ void transactionActions() {
         return;
 
       case 1:
-        withdrawMoney();
+        makeTransactions('withdrawal');
         break;
 
       case 2:
-
+        makeTransactions('deposit');
         break;
 
       case 3:
+        makeTransactions('salary deposit');
         break;
 
       default:
         print("Invalid command. Type again!");
         sleep(const Duration(seconds: 2));
-
     }
   }
 }
@@ -129,8 +132,9 @@ void viewActions() {
   sleep(const Duration(seconds: 2));
 }
 
-
-// Operations
+/*
+ * Operations
+ */
 // Open Account
 void openAccount(int accountType) {
   clearScreen();
@@ -184,7 +188,7 @@ void openSalaryAccount() {
     print("Type the holder's name:");
     holder = stdin.readLineSync()!;
     isHolderAvaible = verifyHolder(holder, 'open-account');
-  } while (isHolderAvaible);
+  } while (!isHolderAvaible);
 
   print("Type the enterprise name:");
   String enterprise = stdin.readLineSync()!;
@@ -199,29 +203,43 @@ void openSalaryAccount() {
   accounts.add(SalaryAccount(holder, balance, enterprise, ein));
 }
 
-// Withdraw and Deposit
-void withdrawMoney() {
+// Withdraw and Deposits
+void makeTransactions(String transactionType) {
   clearScreen();
   bool isHolderAvaible = false;
   String holder = '';
 
-  print("== Making a withdrawal ==");
+  print("== Making a $transactionType ==");
   do {
     print("Type the account's holder:");
     holder = stdin.readLineSync()!;
-    isHolderAvaible = verifyHolder(holder, 'make-withdrawal');
+    isHolderAvaible = verifyHolder(holder, 'make-transaction');
   } while (!isHolderAvaible);
 
   print("Type the value:");
   double value = double.parse(stdin.readLineSync()!);
 
   dynamic account = accounts.firstWhere((ac) => ac.holder == holder);
-  account.send(value);
+  
+  switch (transactionType) {
+    case 'withdrawal':
+      account.send(value);
+      break;
+
+    case 'deposit':
+      account.receive(value);
+      break;
+    
+    case 'salary deposit':
+      account.salaryDeposit(value);
+      break;
+  }
   sleep(const Duration(seconds: 2));
 }
 
-
-// Functions - Verifications
+/*
+ * Functions - Verifications
+ */
 // Verifies if the command is correct
 bool verifyCommand(String value) {
   if (value == '' || value.isEmpty) {
@@ -251,7 +269,7 @@ bool verifyHolder(String value, String prefix) {
     }
   }
 
-  if (prefix == 'make-withdrawal' && !wasFound) {
+  if (prefix == 'make-transaction' && !wasFound) {
     print("Don't found any account with this holder");
     print("Please try again.\n");
     sleep(const Duration(seconds: 2));
@@ -270,8 +288,9 @@ bool checksIfHaveOpenAccounts() {
   return true;
 }
 
-
-// Functions - Others
+/*
+ * Functions - Others
+ */
 // Prints header
 void header() {
   print(" :::====  :::====  :::= === :::  ===");
